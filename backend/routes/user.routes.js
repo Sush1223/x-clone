0fd8e7057b718ff7e0bc1a2ed757c8cfc144a404
Sushant Kalikaprasad Pal <63333545+Sush1223@users.noreply.github.com>
2024-05-22 15:49:43 +0530

    added user controllers and notification controllers

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { protectedRoute } from "../middleware/protectedRoute.js";
+import {
+  getUserProfile,
+  followUnfollowUser,
+  getSuggestedUsers,
+  updateUser,
+} from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.get("/profile/:userName", protectedRoute, getUserProfile);
+router.get("/suggested", protectedRoute, getSuggestedUsers);
+router.post("/follow/:id", protectedRoute, followUnfollowUser);
+router.post("/update", protectedRoute, updateUser);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,27 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectMongoDb from "./db/connectMongoDb.js";
 
 dotenv.config();
-const app = express();
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
+const app = express();
 const PORT = process.env.PORT || 8000;
 // middleware  = a regular function that runs between requests and responses
 app.use(express.json()); //to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 app.use(cookieParser()); // to parse cookies
 app.use("/api/auth", authRoutes);
+// app.use("api/user", userRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
