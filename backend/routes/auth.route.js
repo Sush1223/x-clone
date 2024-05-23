36afeac6f40fe652c96b74b400eb0f53aba62908
Sushant Kalikaprasad Pal <63333545+Sush1223@users.noreply.github.com>
2024-05-23 22:31:19 +0530

    renaming routes file

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -10,9 +10,7 @@ const router = express.Router();
 
 router.get("/me", protectedRoute, getMe);
 router.post("/signup", signup);
-
 router.post("/login", login);
-
 router.post("/logout", logout);
 
 export default router;
