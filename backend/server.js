ea04519fd5ee062582bce3d3c00290127c56b5a0
Sushant Kalikaprasad Pal <63333545+Sush1223@users.noreply.github.com>
2024-06-01 01:20:46 +0530

    ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -17,16 +18,28 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+// the below line is giving use root folder
+const __dirname = path.resolve();
 // middleware  = a regular function that runs between requests and responses
 app.use(express.json({ limit: "5mb" })); //to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 app.use(cookieParser()); // to parse cookies
 
+// when notifications,posts ,users,auth, routes are requested then these below line is going to catch them
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  // other wise below line to catch all kind of routes other than above routes and we basically sendind to react application
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
